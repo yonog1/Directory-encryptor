78ea8ff10e15b0098f78ea561fa341ac6ca3fa06
yonog1 <38842103+yonog1@users.noreply.github.com>
2022-03-22 21:01:18 +0000

    Basic GUI layout
    Basic GUI to showcase the program. Needs the code to be linked to the buttons and text inputs.

---
+++ symmetric_gui.py
@@ -0,0 +1,42 @@
+from cgitb import text
+import kivy
+from kivy.uix.label import Label
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="First Name: "))
+        self.firstName = TextInput(multiline=False)
+        self.inside.add_widget(self.firstName)
+
+        self.inside.add_widget(Label(text="Last Name: "))
+        self.lastName = TextInput(multiline=False)
+        self.inside.add_widget(self.lastName)
+
+        self.inside.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text = "Submit: ", font_size = 40)
+        self.add_widget(self.submit)
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
